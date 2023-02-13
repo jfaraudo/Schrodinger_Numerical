8e52bbb88264a65a8b784dafaa164f5e14e7bc13
Jordi Faraudo <jfaraudo@icmab.es>
2023-02-13 15:12:55 +0100

    Add files via upload

--- HarmonicOscillator/example_harmonic_oscillator.py
+++ HarmonicOscillator/example_harmonic_oscillator.py
@@ -4,43 +4,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-#Physical constants in atomic units
-hbar = 1
-
-#Physical constants and parameters
-m = 1.0
-omega = 1.0
-
-#General Options for the calculation
-L = 20
-
 #Solver Options
-#grid size (how many discrete points to use in the range [-10,10])
-npoints=1000
+L = 20 # Interval for calculating the wave function [-L/2,L/2]
+npoints=1000 #grid size (how many discrete points to use in the range [-L/2,L/2])
 
-#Quantum state
-#v=0
-v=1
-#v = int(input("\n Quantum Number (enter 0 for ground state):\n>"))
-print("Please wait")
-
-#Define self
-#solver = PDE_BV(m1,omega1, L1, n)
-#self.m = m
-#self.omega = omega
-#self.L = L
-xlower = -L*0.5
-xupper = L*0.5
-#self.npoints = npoints
+#Domain at which the solution will be calculated
+xlower = -L/2.0
+xupper = L/2.0
 x = np.linspace(xlower,xupper,npoints)
-h = x[1]-x[0]
+h = x[1]-x[0]  #discretization in space
+
+#Init Energy and Wavefunction
 psi = [None]*npoints
 E = np.zeros(npoints)
 
+print("Please wait")
 
 #Potential as a function of position
 def getV(x):
-        return 0.5*m*omega**2*x**2
+        potvalue = (1.0/2.0)*x**2 
+        return potvalue
 
 def getUii(i):
         return  -2*(h**2)*getV(x[i])
@@ -50,41 +33,50 @@ def getUii(i):
 #-------------------------
 
 #Calculation of F
+print("Calculating matrix...")
 F = np.zeros([npoints,npoints])
 for i in range(0,npoints):
-	F[i,i] = getUii(i) - 2/m
+	F[i,i] = getUii(i) - 2
 	if i > 0:
-		F[i,i-1] = 1/m
+		F[i,i-1] = 1
 		if i < npoints-1:
-			F[i,i+1] = 1/m
+			F[i,i+1] = 1
 
-#diagonalize
-# w eigenvalues and v eigenvectors
-#w,vs = np.linalg.eig(F)
+#diagonalize the matrix F
+print("Diagonalizing...")
 eigenValues, eigenVectors = np.linalg.eig(F)
 
-#Order results by energy value
+#Order results by eigenvalue
+# w ordered eigenvalues and vs ordered eigenvectors
 idx = eigenValues.argsort()[::-1]   
 w = eigenValues[idx]
 vs = eigenVectors[:,idx]
 
-#Energy
-E = - w/2.0/h**2
-#print("Eigenvalues:",w)
-print("Ground and ten first Energy levels (in Hartree):")
-print(E[0],E[1],E[2],E[3],E[4])
+#Energy Level
+E = - w/(2.0*h**2)
 
+#Print Energy Values
+print("RESULTS:")
+for k in range(0,9):
+	print("State ",k," Energy = %.2f" %E[k])
+
+#Calculation of normalised Wave Functions
 for k in range(0,len(w)):
 	psi[k] = vs[:,k]
 	integral = h*np.dot(psi[k],psi[k])
 	psi[k] = psi[k]/integral**0.5
 
+#Plot Wave functions
 print("Plotting")
-#print(psi[v])
-plt.plot(x,psi[v],label=r'$\psi_v(x)$, k = ' + str(v))
-#plt.plot(x,psi[v])
-plt.title(r'$v=$'+ str(v) + r', $E_v$=' + '{:10.4f}'.format(E[v]))
-plt.legend()
-plt.xlabel(r'$x$(bohr)')
-plt.ylabel(r'$\psi(x)$')
-plt.show()
+
+#v = int(input("\n Quantum Number (enter 0 for ground state):\n>"))
+for v in range(0,9):
+	plt.plot(x,psi[v],label=r'$\psi_v(x)$, k = ' + str(v))
+	#plt.plot(x,psi[v])
+	plt.title(r'$n=$'+ str(v) + r', $E_n$=' + '{:.2f}'.format(E[v]))
+	plt.legend()
+	plt.xlabel(r'$x$(dimensionless)')
+	plt.ylabel(r'$\psi(x)$')
+	plt.show()
+
+print("Bye")
